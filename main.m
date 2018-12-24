4771aa7f21df900db8df906312ca01e5b523a856
Katasuke <46043841+Katasuke@users.noreply.github.com>
2018-12-24 12:10:31 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,15 +1,16 @@
-video = VideoReader('C:\ICM_project\video\testVideo.mp4');
+video = VideoReader('C:\ICM_project\video\testVideo2.mp4');
 movie = getStructure(video);
 
 
 background = getBackground(movie,20,10);
 %find the ball
 for k = 1:movie.nFrames
-[ balls, difference ] = detectBall(movie.mov(k).gray,background,0,1,10);
+[ balls, difference ] = detectBall(movie.mov(k).gray,background,0.01,100,30);
 movie.mov(k).balls = balls;
 movie.mov(k).difference = difference;
-
 end
+
+
 
 makevideo(movie, 'testFunction_difference.avi', 'testFunction_balls.avi');
 
