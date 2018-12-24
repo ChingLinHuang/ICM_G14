ae159dede60e43b861426c29a46d6e026084ab5a
Katasuke <46043841+Katasuke@users.noreply.github.com>
2018-12-24 17:35:03 +0800

    Update main.m

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ movie = getStructure(video);
 background = getBackground(movie,20,10);
 %find the ball
 for k = 1:movie.nFrames
-[ balls, difference ] = detectBall(movie.mov(k).gray,background,0.01,100,30);
+[ balls, difference ] = detectBall(movie.mov(k).gray,background,0.7,80,800,10);
 movie.mov(k).balls = balls;
 movie.mov(k).difference = difference;
 end
