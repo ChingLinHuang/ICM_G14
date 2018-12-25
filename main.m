1862420ff6605f55340912b41672b637ccf61c04
a2831408 <46098877+a2831408@users.noreply.github.com>
2018-12-26 00:33:11 +0800

    Update main.m

--- main.m
+++ main.m
@@ -7,14 +7,14 @@ background = getBackground(movie,20,10);
  
 %find the ball
 for k = 1:movie.nFrames
-[ balls, position ] = detectBall(movie.mov(k).gray,background,0.1,25,800,10,80);
+[ balls, position ] = detectBall(movie.mov(k).gray,background,1.5,80,800,10);
 movie.mov(k).balls = balls;
 movie.mov(k).position = position;
 %movie.mov(k).difference = difference;
 end
 
 makevideo(movie, 'testFunction_balls.avi');
-movie = removeOthers(movie,800);
+movie = removeOthers(movie,100);
 
 
 
