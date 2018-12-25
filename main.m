24718244dcbf79e0d83565e2eaf84e2bd07af569
Katasuke <46043841+Katasuke@users.noreply.github.com>
2018-12-25 19:46:06 +0800

    Update main.m

--- main.m
+++ main.m
@@ -3,14 +3,21 @@ movie = getStructure(video);
 
 
 background = getBackground(movie,20,10);
+
+ 
 %find the ball
 for k = 1:movie.nFrames
-[ balls, difference ] = detectBall(movie.mov(k).gray,background,0.7,80,800,10);
+[ balls, position ] = detectBall(movie.mov(k).gray,background,0.1,25,800,10,80);
 movie.mov(k).balls = balls;
-movie.mov(k).difference = difference;
+movie.mov(k).position = position;
+%movie.mov(k).difference = difference;
 end
 
+makevideo(movie, 'testFunction_balls.avi');
+movie = removeOthers(movie,800);
 
 
-makevideo(movie, 'testFunction_difference.avi', 'testFunction_balls.avi');
+
+
+makevideo(movie, 'testFunction_balls_removed.avi');
 
