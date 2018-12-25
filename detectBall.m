36ce4911799dd1073f5d125c19efdc9d832e7adf
Katasuke <46043841+Katasuke@users.noreply.github.com>
2018-12-25 20:23:32 +0800

    Add files via upload

--- detectBall.m
+++ detectBall.m
@@ -1,8 +1,9 @@
-function [ balls, position] = detectBall( frame, background, f, pix_min, pix_max,nd, med_inf)
+function [ balls, position] = detectBall( frame, background, f, pix_min1,pix_min2, pix_max,nd, med_inf)
 %test
 %frame = movie.mov(58).gray;
 %f=0.7;
-%pix_min = 80;
+%pix_min1 = 80;
+%pix_min2 = 50;
 %pix_max = 800;
 %nd =10;
 %med_inf = 400;
@@ -14,18 +15,20 @@ dif = double(difference).^2-differenceMed^2;
 difMed = mean(mean(dif));
 
 bw = (dif>difMed*f);
-%test1=uint8(bw*255);
+test1=uint8(bw*255);
 %remove small objects
-bw = bwareaopen(bw, pix_min);
-%test2=uint8(bw*255);
+bw = bwareaopen(bw, pix_min1);
+test2=uint8(bw*255);
 
 SE = strel('disk', nd);
 bw = imclose(bw, SE);
-%test3 = bw;
+test3 = bw;
+bw = bwareaopen(bw, pix_min2);
+test31=uint8(bw*255);
 
 %remove big objects
 bw = bw - bwareaopen(bw,pix_max);
-%test4 = bw;
+test4 = bw;
 
 [balls, nCG] = bwlabeln(bw);
 
@@ -46,5 +49,3 @@ else
     position =[0 0];
     balls = (balls == balls)*0;
 end
-%imshow(movie.mov(60).rgb)
-%imshow(balls)
--- removeOthers.m
+++ removeOthers.m
@@ -1,15 +1,15 @@
 %function [disp,movie] = showDisp(movie)
 function [movie] = removeOthers(movie,remov_disp)
-%remov_disp  = 650
+%remov_disp  = 700;
 
 disp = [];
 
 
 for k = 1:movie.nFrames-2
 
-    disp1 = [disp norm(movie.mov(k).position-movie.mov(k+1).position)];
-    disp2 = [disp norm(movie.mov(k).position-movie.mov(k+2).position)];
-    if (disp1 > remov_disp)&(disp2>remov_disp)
+    disp = [disp norm(movie.mov(k).position-movie.mov(k+1).position)];
+
+    if disp(end) > remov_disp
         movie.mov(k).balls = movie.mov(k).balls*0;
     end
 end
